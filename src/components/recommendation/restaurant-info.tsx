4a9b74d7d973640ba572b5d5c74bf63eea75dd47
mokhs00 <mokhs00@naver.com>
2024-10-23 02:34:39 +0900

    Update 음식점 영업 시간 추가 및 설명 데이터는 보류

--- src/components/recommendation/restaurant-info.tsx
+++ src/components/recommendation/restaurant-info.tsx
@@ -1,5 +1,5 @@
 import { makeDayOfWeek, RestaurantRecommendation } from "@/api/api";
-import { makeDistance, makePriceRangePerPerson } from "@/domain/restaurant";
+import { makeDistance, makeOpenTimeToday, makePriceRangePerPerson } from "@/domain/restaurant";
 import Image from "next/image";
 
 type Props = {
@@ -7,7 +7,6 @@ type Props = {
 }
 
 export const RestaurantInfo = ({ restaurant }: Props) => {
-  const now = new Date();
 
   return (
     <>
@@ -21,9 +20,10 @@ export const RestaurantInfo = ({ restaurant }: Props) => {
       </div>
 
       <div className="mt-4 text-gray-700 text-sm">
-        <p className="text-primary font-semibold">
+        {/* TOOD: 음식점 설명 데이터 파이프라인 작업 후 추가 */}
+        {/* <p className="text-primary font-semibold">
           {restaurant.description}
-        </p>
+        </p> */}
         <div className="flex space-x-2 mt-2">
           <h2 className="text-xl font-bold text-secondary">
             {restaurant.name}
@@ -32,11 +32,13 @@ export const RestaurantInfo = ({ restaurant }: Props) => {
             {makeDistance(restaurant.distanceInMeters)}
           </p>
         </div>
-        <p className="mt-1 font-semibold">
-          {makePriceRangePerPerson(restaurant.minimumPricePerPerson, restaurant.maximumPricePerPerson)}
-          {restaurant.businessHours.find(
-            (businessHour) => businessHour.dayOfWeekEnum === makeDayOfWeek(now)
-          )?.openTime}{" "}
+        <p className="mt-1 font-semibold flex">
+          <div className="mr-1">
+            {makePriceRangePerPerson(restaurant.minimumPricePerPerson, restaurant.maximumPricePerPerson)}
+          </div>
+          <div>
+            {makeOpenTimeToday(restaurant.businessHours)}
+          </div>
         </p>
       </div>
     </>
--- src/domain/restaurant/index.ts
+++ src/domain/restaurant/index.ts
@@ -1,3 +1,4 @@
+import { BusinessHour, makeDayOfWeek } from "@/api/api";
 import { formatNumberWithCommas } from "@/utils/number";
 
 export const makePriceRangePerPerson = (
@@ -12,4 +13,24 @@ export const makePriceRangePerPerson = (
 export const makeDistance = (distanceInMeters: number): string => {
   // 인간이 100m 를 걷는 시간 평균 72초 -> 80m를 1분으로 계산
   return `${distanceInMeters}m(도보 ${Math.ceil(distanceInMeters / 80)}분)`;
+};
+
+export const makeOpenTimeToday = (
+  businessHours: BusinessHour[]
+): string | undefined => {
+  const now = new Date();
+
+  const businessHourToday = businessHours.find(
+    (businessHour) => businessHour.dayOfWeekEnum === makeDayOfWeek(now)
+  );
+
+  if (businessHourToday === undefined) {
+    return undefined;
+  }
+
+  if (businessHourToday.isClosedDay) {
+    return "오늘 휴무";
+  }
+
+  return `${businessHourToday.openTime}~${businessHourToday.closingTime}`;
 };
